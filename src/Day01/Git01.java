bb2049ef514cb41d1d829de208e706b42bb6769e
seyfi <giraykut01@mail.com>
2022-08-16 12:08:59 +0300

    v1

---
+++ src/Day01/Git01.java
@@ -0,0 +1,29 @@
+package Day01;
+
+public class Git01 {
+}
+    /*İlk defa PUSH yapacaksanız bir projeyi ;
+
+1 - Önce GitHub a gidip New Repository i oluşturun
+        2- Oluşturduğunuz repository de size verilen https://github.com/.............git   adresini kopyalayın
+
+
+        Şimdi IntelliJ ye gelip TERMINAL ı açın;
+
+        3- git init yazın
+        4- git add . yazın
+        5- git commit -m"first commit"  (first commit yerine  ilke versiyon, ilk ders veya istediğiniz bir şeyi yazın)
+        6- git branch -M main
+        7- git remote add origin https://github.com/.............git (yukarıda2. satırda kopyalağınız adresi buraya yapıştırın)
+        8- git push -u origin main
+
+
+        SONRASINDA Aynı PROJE için ;
+
+        her yeni eklediğiniz şeyden sonra GitHub'a yüklemek için
+
+        9- git add .
+        10 - git commit -m"YENI NUMARA VEYA YAZI YAZ"
+        11 - git push
+
+     */
